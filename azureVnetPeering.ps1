079a31d334eecf841efe3b011e21f4731dd6ede7
Amrutha-Veedhi <amrutha.veedhi@netenrich.com>
2019-01-30 16:04:11 +0530

    First Commit

---
+++ azureVnetPeering.ps1
@@ -0,0 +1,9 @@
+﻿#Peering between Azure VNETS
+Login-AzureRmAccount
+$resourceGroup = read-host "Enter your ResourceGroup:::"
+$vnet1Name = Read-host "Enter your VNET1 Name:::"
+$vnet2Name = read-host "Enter Your VNET2 Name"
+$vnet1 = Get-AzureRmVirtualNetwork -ResourceGroupName $resourceGroup -Name $vnet1Name
+$vnet2 = Get-AzureRmVirtualNetwork -ResourceGroupName $resourceGroup -Name $vnet2Name
+Add-AzureRmVirtualNetworkPeering -Name LinkToVNet2 -VirtualNetwork $vnet1 -RemoteVirtualNetworkId $vnet2.Id
+Add-AzureRmVirtualNetworkPeering -Name LinkToVNet1 -VirtualNetwork $vnet2 -RemoteVirtualNetworkId $vnet1.Id
